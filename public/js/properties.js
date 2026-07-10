feb10441534730fafa2fa1dc66eedeb70dd841ed
Sharoff <programminggenius2006@gmail.com>
2026-07-10 17:23:40 +0530

    Altair 01

--- public/js/properties.js
+++ public/js/properties.js
@@ -577,7 +577,6 @@ A rare opportunity to own a luxury apartment that perfectly combines stunning vi
       './Images/altair-01/8.jpeg',
       './Images/altair-01/9.jpeg',
       './Images/altair-01/10.jpeg',
-      './Images/altair-01/11.jpeg',
     ],
     description: `Luxury 3 Bedroom Apartment for Sale | Altair | Colombo 02
 
